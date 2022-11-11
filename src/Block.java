b0063d363e9daab09e9ee0803027d22e3ad1ec62
Anjanay Raina <anjanayraina326@gmail.com>
2022-11-11 21:48:44 +0530

    Block Created

--- src/Block.java
+++ src/Block.java
@@ -2,5 +2,13 @@ import java.sql.Timestamp;
 
 public class Block {
     Timestamp timestamp;
-    Block()
+    String prevHash;
+    String currentHash;
+    String data;
+    Block(String currentHash , String prevHash , String data){
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+        this.prevHash = prevHash;
+        this.currentHash =currentHash;
+        this.data = data;
+    }
 }
