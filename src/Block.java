f06003456cd9004142a9676103eacf280a55deae
Anjanay Raina <anjanayraina326@gmail.com>
2022-11-13 19:30:35 +0530

    SHA256 Tested

--- src/Block.java
+++ src/Block.java
@@ -1,14 +1,16 @@
 import java.sql.Timestamp;
 
 public class Block {
+    long index;
     Timestamp timestamp;
     String prevHash;
     String currentHash;
     String data;
-    Block(String currentHash , String prevHash , String data){
+    Block(String currentHash , String prevHash , String data , long index){
         this.timestamp = new Timestamp(System.currentTimeMillis());
         this.prevHash = prevHash;
         this.currentHash =currentHash;
         this.data = data;
+        this.index = index;
     }
 }
