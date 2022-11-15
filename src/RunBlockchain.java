f241dc010b4cb07696b1d7ef257677ec76b20e08
Anjanay Raina <anjanayraina326@gmail.com>
2022-11-15 12:36:00 +0530

    Runnable code in the making

--- src/RunBlockchain.java
+++ src/RunBlockchain.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class RunBlockchain {
-
+    public Node createAccount()
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Welcome to your wallet!!" +
@@ -9,6 +9,16 @@ public class RunBlockchain {
         ProgramRunner runner = new ProgramRunner();
         runner.printTasks();
         int ch = scn.nextInt();
+        if(ch == 1){
+
+        }
+        else if(ch == 2){
+
+        }
+
+        else{
+            System.out.println("please enter a valid entry!");
+        }
 
 
 
